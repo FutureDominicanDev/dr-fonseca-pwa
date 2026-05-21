625cee28c9a7d35beb8f5b5c37a1d3483f0b03eb
Ray <mrdiazsr@icloud.com>
2026-05-20 17:41:54 -0700

    Force Android portrait and keep iPad rotation

--- android/app/src/main/java/com/drfonsecacirujanoplastico/portal/MainActivity.java
+++ android/app/src/main/java/com/drfonsecacirujanoplastico/portal/MainActivity.java
@@ -1,12 +1,20 @@
 package com.drfonsecacirujanoplastico.portal;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         registerPlugin(PortalNotificationSettingsPlugin.class);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
