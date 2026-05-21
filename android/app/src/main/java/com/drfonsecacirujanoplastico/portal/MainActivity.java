61ef94b503a707761fc0f0b45ffff7fbab4ab64c
Ray <mrdiazsr@icloud.com>
2026-05-20 21:22:04 -0700

    Hide Android status bar in native shell

--- android/app/src/main/java/com/drfonsecacirujanoplastico/portal/MainActivity.java
+++ android/app/src/main/java/com/drfonsecacirujanoplastico/portal/MainActivity.java
@@ -2,7 +2,12 @@ package com.drfonsecacirujanoplastico.portal;
 
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -10,22 +15,51 @@ public class MainActivity extends BridgeActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
+    private void hideSystemStatusBar() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowInsetsController controller = getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         lockPortraitOrientation();
+        hideSystemStatusBar();
         registerPlugin(PortalNotificationSettingsPlugin.class);
         super.onCreate(savedInstanceState);
+        hideSystemStatusBar();
     }
 
     @Override
     public void onResume() {
         super.onResume();
         lockPortraitOrientation();
+        hideSystemStatusBar();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         lockPortraitOrientation();
+        hideSystemStatusBar();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemStatusBar();
+        }
     }
 }
